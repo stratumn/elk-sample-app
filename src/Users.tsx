f920aecb8ba412577fc28e7a8a222e114e0ab33d
t-bast <bastien@stratumn.com>
2019-01-24 15:18:18 +0100

    Add users dynamically

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,8 +1,11 @@
 import React, { PureComponent } from 'react';
+import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
 
 class State {
   readonly users: string[] = [];
+  readonly modalIsOpen: boolean = false;
+  readonly modalUserName: string = '';
 }
 
 export class Users extends PureComponent<{}, State> {
@@ -14,6 +17,36 @@ export class Users extends PureComponent<{}, State> {
       users: ['alice', 'bob']
     });
   }
+
+  openUserModal = () => {
+    this.setState({
+      modalIsOpen: true
+    });
+  };
+
+  closeUserModal = () => {
+    this.setState({
+      modalIsOpen: false,
+      modalUserName: ''
+    });
+  };
+
+  updateModalUserName = (name: string) => {
+    this.setState({
+      modalUserName: name
+    });
+  };
+
+  addUser = () => {
+    const users = Object.assign([], this.state.users);
+    users.push(this.state.modalUserName);
+
+    this.setState({
+      modalIsOpen: false,
+      modalUserName: '',
+      users
+    });
+  };
 
   render() {
     const { users } = this.state;
@@ -36,6 +69,40 @@ export class Users extends PureComponent<{}, State> {
             </li>
           ))}
         </ul>
+        <button onClick={this.openUserModal}>Add user</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          contentLabel="Add User"
+          onRequestClose={this.closeUserModal}
+        >
+          <div>
+            <h1>Add User</h1>
+            <div style={{ margin: '20px' }}>
+              <label>
+                <span>Name: </span>
+                <input
+                  type="text"
+                  value={this.state.modalUserName}
+                  onChange={e => this.updateModalUserName(e.target.value)}
+                />
+              </label>
+            </div>
+            <div style={{ margin: '20px' }}>
+              <button
+                onClick={this.addUser}
+                style={{ margin: '5px', padding: '5px' }}
+              >
+                Add
+              </button>
+              <button
+                onClick={this.closeUserModal}
+                style={{ margin: '5px', padding: '5px' }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+
+Modal.setAppElement('#root');
 
 ReactDOM.render(
   <BrowserRouter>
